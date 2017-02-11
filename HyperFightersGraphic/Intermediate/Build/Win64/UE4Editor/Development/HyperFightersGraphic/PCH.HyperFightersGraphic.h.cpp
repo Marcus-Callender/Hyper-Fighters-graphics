8d3c57a031221d2d42c5f98da8fb31dd33f441d4
Marcus Callender <m.shute@outlook.com>
2017-02-11 16:59:53 +0000

    Initial commit for unreal project files

---
+++ HyperFightersGraphic/Intermediate/Build/Win64/UE4Editor/Development/HyperFightersGraphic/PCH.HyperFightersGraphic.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\User\Desktop\HyperFightersGraphic\Source\HyperFightersGraphic\HyperFightersGraphic.h"
---
+++ HyperFightersGraphic/Source/HyperFightersGraphic/HyperFightersGraphicGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "HyperFightersGraphicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HYPERFIGHTERSGRAPHIC_API AHyperFightersGraphicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
