e6d5ec6f3b4858ae3d226392324fef524671186b
Marcus Callender <m.shute@outlook.com>
2017-02-12 18:35:30 +0000

    Created a new project to add the starter content

--- HyperFightersGraphic/Intermediate/Build/Win64/UE4Editor/Development/HyperFightersGraphic/HyperFightersGraphic.h
+++ HyperFightersGraphic/Intermediate/Build/Win64/UE4Editor/Development/HyperFightersGraphic/HyperFightersGraphic.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_HYPERFIGHTERSGRAPHIC_H__
 #define __AUTO_HYPERFIGHTERSGRAPHIC_H__
-//Last Write: 11/02/2017 16:07:44 +00:00
+//Last Write: 12/02/2017 18:18:10 +00:00
 #include "C:\Users\User\Desktop\Hyper-Fighters-graphics\HyperFightersGraphic\Source\HyperFightersGraphic\HyperFightersGraphic.h"
 #endif//__AUTO_HYPERFIGHTERSGRAPHIC_H__
